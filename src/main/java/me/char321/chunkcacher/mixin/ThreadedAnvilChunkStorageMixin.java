608f8b919d39e9fd9b8ad10d11e9eedc9fd6a700
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-09-30 12:50:04 +0200

    use inject instead of overwrite

--- src/main/java/me/char321/chunkcacher/mixin/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/me/char321/chunkcacher/mixin/ThreadedAnvilChunkStorageMixin.java
@@ -3,24 +3,14 @@ package me.char321.chunkcacher.mixin;
 import com.mojang.datafixers.util.Either;
 import me.char321.chunkcacher.WorldCache;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ChunkHolder;
-import net.minecraft.server.world.ChunkTaskPrioritySystem;
-import net.minecraft.server.world.ServerLightingProvider;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
-import net.minecraft.structure.StructureManager;
-import net.minecraft.util.crash.CrashException;
-import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.thread.MessageListener;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -30,62 +20,17 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.IntFunction;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class ThreadedAnvilChunkStorageMixin {
-    @Shadow @Final private WorldGenerationProgressListener worldGenerationProgressListener;
-
-    @Shadow @Final private ChunkGenerator chunkGenerator;
-
-    @Shadow @Final private MessageListener<ChunkTaskPrioritySystem.Task<Runnable>> worldGenExecutor;
-
-    @Shadow protected abstract void releaseLightTicket(ChunkPos pos);
 
     @Shadow @Final private ServerWorld world;
 
-    @Shadow @Final private StructureManager structureManager;
-
-    @Shadow @Final private ServerLightingProvider serverLightingProvider;
-
-    @Shadow protected abstract CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> convertToFullChunk(ChunkHolder chunkHolder);
-
-    @Shadow protected abstract CompletableFuture<Either<List<Chunk>, ChunkHolder.Unloaded>> getRegion(ChunkPos centerChunk, int margin, IntFunction<ChunkStatus> distanceToStatus);
-
-    @Shadow protected abstract ChunkStatus getRequiredStatusForGeneration(ChunkStatus centerChunkTargetStatus, int distance);
-
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    private CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> upgradeChunk(ChunkHolder holder, ChunkStatus requiredStatus) {
-        ChunkPos chunkPos = holder.getPos();
-        CompletableFuture<Either<List<Chunk>, ChunkHolder.Unloaded>> completableFuture = this.getRegion(chunkPos, requiredStatus.getTaskMargin(), i -> this.getRequiredStatusForGeneration(requiredStatus, i)) ;
-        this.world.getProfiler().visit(() -> "chunkGenerate " + requiredStatus.getId());
-        return completableFuture.thenComposeAsync(either -> either.map(list -> {
-            try {
-                CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> future2 = requiredStatus.runGenerationTask(this.world, this.chunkGenerator, this.structureManager, this.serverLightingProvider, chunk -> this.convertToFullChunk(holder), (List<Chunk>)list);
-                if (WorldCache.shouldCache() && future2.isDone() && !requiredStatus.isAtLeast(ChunkStatus.FEATURES)) {
-                    future2.getNow(null).ifLeft((chunk) -> {
-                        WorldCache.addChunk(chunkPos, chunk, world);
-                    });
-                }
-                this.worldGenerationProgressListener.setChunkStatus(chunkPos, requiredStatus);
-                return future2;
-            }
-            catch (Exception exception) {
-                CrashReport crashReport = CrashReport.create(exception, "Exception generating new chunk");
-                CrashReportSection crashReportSection = crashReport.addElement("Chunk to be generated");
-                crashReportSection.add("Location", String.format("%d,%d", chunkPos.x, chunkPos.z));
-                crashReportSection.add("Position hash", ChunkPos.toLong(chunkPos.x, chunkPos.z));
-                crashReportSection.add("Generator", this.chunkGenerator);
-                throw new CrashException(crashReport);
-            }
-        }, unloaded -> {
-            this.releaseLightTicket(chunkPos);
-            return CompletableFuture.completedFuture(Either.right(unloaded));
-        }), runnable -> this.worldGenExecutor.send(ChunkTaskPrioritySystem.createMessage(holder, runnable)));
+    @Inject(method = "method_17225", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/WorldGenerationProgressListener;setChunkStatus(Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/world/chunk/ChunkStatus;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void addToCache(ChunkPos chunkPos, ChunkHolder chunkHolder, ChunkStatus chunkStatus, List list, CallbackInfoReturnable<CompletableFuture> cir, CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>> completableFuture) {
+        if (WorldCache.shouldCache() && completableFuture.isDone() && !chunkStatus.isAtLeast(ChunkStatus.FEATURES)) {
+            completableFuture.getNow(null).ifLeft((chunk) -> WorldCache.addChunk(chunkPos, chunk, world));
+        }
     }
 
     @ModifyVariable(method = "getUpdatedChunkNbt", at = @At(value="STORE"))
