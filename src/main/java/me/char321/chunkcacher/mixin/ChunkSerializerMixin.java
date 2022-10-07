efa3c5938599188a54fa502cd2fefc25a8ecd7ba
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-10-07 14:21:56 +0200

    cleanup ChunkSerializerMixin
    redirect getHeightmapTypes() instead of iterator()
    and remove unused imports

--- src/main/java/me/char321/chunkcacher/mixin/ChunkSerializerMixin.java
+++ src/main/java/me/char321/chunkcacher/mixin/ChunkSerializerMixin.java
@@ -1,29 +1,19 @@
 package me.char321.chunkcacher.mixin;
 
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtLongArray;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.Heightmap;
-import net.minecraft.world.TickScheduler;
-import net.minecraft.world.biome.source.BiomeArray;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.ProtoChunk;
-import net.minecraft.world.chunk.UpgradeData;
-import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.EnumSet;
-import java.util.Iterator;
 
 @Mixin(ChunkSerializer.class)
 public class ChunkSerializerMixin {
@@ -37,8 +27,8 @@ public class ChunkSerializerMixin {
         return nbtCompound;
     }
 
-    @Redirect(method = "deserialize", at = @At(value = "INVOKE", target = "Ljava/util/EnumSet;iterator()Ljava/util/Iterator;"))
-    private static Iterator<Heightmap.Type> deserializeHeightmaps(EnumSet instance) {
-        return EnumSet.allOf(Heightmap.Type.class).iterator();
+    @Redirect(method = "deserialize", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/ChunkStatus;getHeightmapTypes()Ljava/util/EnumSet;"))
+    private static EnumSet<Heightmap.Type> deserializeHeightmaps(ChunkStatus instance) {
+        return EnumSet.allOf(Heightmap.Type.class);
     }
 }
