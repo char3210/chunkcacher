92d56b900cee942014436194165784b80f2f5716
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-10-28 23:01:39 +0200

    readd checkGeneratorOptions()
    replaced the chunkgenerator.getclass()-check with just checking isFlatWorld() because it does the same thing
    removed hasbonuschest because its a feature

--- src/main/java/me/char321/chunkcacher/WorldCache.java
+++ src/main/java/me/char321/chunkcacher/WorldCache.java
@@ -9,12 +9,14 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.GeneratorOptions;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class WorldCache {
     public static boolean isGenerating = false;
+    private static GeneratorOptions lastGeneratorOptions;
     private static final Map<RegistryKey<World>, Long2ObjectLinkedOpenHashMap<NbtCompound>> cache = new HashMap<>();
 
     public static void addChunk(ChunkPos chunkPos, Chunk chunk, ServerWorld world) {
@@ -29,6 +31,24 @@ public class WorldCache {
         Long2ObjectLinkedOpenHashMap<NbtCompound> map = cache.get(world.getRegistryKey());
         if (map == null) return null;
         return map.get(chunkPos.toLong());
+    }
+
+    /**
+     * Checks if the generator options have changed, if so, clear the cache
+     * dude github copilot is so cool it auto generated these comments
+
+     * kept as fallback just in case some Atum update messes anything up
+     * not perfect but good enough for that purpose
+     */
+    public static void checkGeneratorOptions(GeneratorOptions generatorOptions) {
+        if (lastGeneratorOptions == null ||
+                lastGeneratorOptions.getSeed() != generatorOptions.getSeed() ||
+                lastGeneratorOptions.shouldGenerateStructures() != generatorOptions.shouldGenerateStructures() ||
+                lastGeneratorOptions.isFlatWorld() != generatorOptions.isFlatWorld()
+        ) {
+            clearCache();
+            lastGeneratorOptions = generatorOptions;
+        }
     }
 
     public static void clearCache() {
--- src/main/java/me/char321/chunkcacher/mixin/MinecraftServerMixin.java
+++ src/main/java/me/char321/chunkcacher/mixin/MinecraftServerMixin.java
@@ -2,7 +2,10 @@ package me.char321.chunkcacher.mixin;
 
 import me.char321.chunkcacher.WorldCache;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.SaveProperties;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,9 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
 
+    @Shadow @Final protected SaveProperties saveProperties;
+
     @Inject(method = "createWorlds", at = @At("HEAD"))
     private void isGenerating(CallbackInfo ci) {
         WorldCache.isGenerating = true;
+        WorldCache.checkGeneratorOptions(this.saveProperties.getGeneratorOptions());
     }
 
     @Inject(method = "loadWorld", at = @At("TAIL"))
