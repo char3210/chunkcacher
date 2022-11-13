9a963a35c77c7942a84b9592fe818436afeaa9f9
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-11-13 11:20:30 +0100

    1.19 compat & bring code up to date

--- src/main/java/me/char321/chunkcacher/mixin/TitleScreenMixin.java
+++ src/main/java/me/char321/chunkcacher/mixin/TitleScreenMixin.java
@@ -10,9 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
+
     @Inject(method = "init", at = @At("HEAD"))
-    public void clearCacheOnAtumQuit(CallbackInfo ci) {
-        if (!Atum.isRunning) {
+    private void clearCacheOnAtumQuit(CallbackInfo ci) {
+        if (!Atum.isRunning || Atum.seed == null || Atum.seed.isEmpty()) {
             WorldCache.clearCache();
         }
     }
